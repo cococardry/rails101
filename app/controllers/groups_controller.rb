eb63c7a679819088304ef7700931c5f87bb33c53
coco <cococardry@163.com>
2017-04-13 00:09:23 +0800

    install bullet gem and modified development.rb & controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,7 +3,8 @@ class GroupsController < ApplicationController
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
-    @groups = Group.all
+    # @groups = Group.all
+    @groups=Group.includes(:user).all
   end
 
   def new
@@ -23,7 +24,7 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     @group.user = current_user
     if @group.save
-      current_user.join!(@group)  
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render :new
